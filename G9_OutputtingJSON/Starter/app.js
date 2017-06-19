5c67af8ab4de81df050e9194ed7f08167b8a721b
hwalborn <hwalborn@gmail.com>
2017-06-19 16:29:23 -0400

    practice making API with node

---
+++ G9_OutputtingJSON/Starter/app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  let obj = {
+    firstname: "Fake",
+    lastname: "Faker"
+  }
+  res.end(JSON.stringify(obj))
+}).listen(1337, '127.0.0.1')
